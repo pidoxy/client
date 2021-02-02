4dee08e16eb5f8ab038b5ce4fad7b6d9286a8d63
IDOKO EMMANUEL <pidox31102002@gmail.com>
2021-02-02 01:15:55 +0100

    pic

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,8 +1,8 @@
 import React from "react";
-import violin from "./violin.svg";
+import violin from "../violin.svg";
 
 const OurServices = ({ bgColor }) => {
-
+console.log(violin);
     return (
         <div className={` ${bgColor} services-page mb-3`}>
             <h2>Our Services</h2>
@@ -10,7 +10,7 @@ const OurServices = ({ bgColor }) => {
                 <div className="row g-5">
                     <div className="col-md-6 col-12 mb-3 mb-0-md">
                         <div className="card  p-3">
-                            <img src="" class="card-img-top img-thumbnail img-fluid" alt="..." />
+                            <img src={violin} class="card-img-top img-fluid" alt="violin" />
                             <div className="card-body">
                                 <h5 className="card-title text-center">GRACEWORTHS MUSICAL SCHOOL</h5>
                                 <p className="card-text">We understake training for the following instruments: Recorder, trumpet, violin, saxophone, flute, guitar</p>
@@ -19,7 +19,7 @@ const OurServices = ({ bgColor }) => {
                     </div>
                     <div className="col-md-6 col-12 mb-3 mb-0-md">
                         <div className="card  p-3">
-                            <img src="" class="card-img-top img-thumbnail img-fluid" alt="..." />
+                            <img src={violin} class="card-img-top img-fluid" alt="violin" />
                             <div className="card-body">
                                 <h5 className="card-title text-center">GRACEWORTHS VOICE AND ALBUM</h5>
                                 <p className="card-text">We undertake sessions on voice training and Album creation...all are certified courses and professional examinations are available.</p>
@@ -28,7 +28,7 @@ const OurServices = ({ bgColor }) => {
                     </div>
                     <div class="col-md-6 col-12 mb-3 mb-0-md">
                         <div className="card  p-3">
-                            <img src="" class="card-img-top img-thumbnail img-fluid" alt="..." />
+                            <img src={violin} class="card-img-top img-fluid" alt="violin" />
                             <div className="card-body">
                                 <h5 className="card-title text-center">GRACEWORTHS LOCAL/INTERNATIONAL LANGUAGES</h5>
                                 <p className="card-text">title and make up the bulk of the card's content.</p>
@@ -37,7 +37,7 @@ const OurServices = ({ bgColor }) => {
                     </div>
                     <div class="col-md-6 col-12 mb-3 mb-0-md">
                         <div className="card  p-3">
-                            <img src={violin} class="card-img-top img-thumbnail img-fluid" alt="violin" />
+                            <img src={violin} class="card-img-top img-fluid" alt="violin" />
                             <div className="card-body">
                                 <h5 className="card-title text-center">VOCATION ASPECT</h5>
                                 <p className="card-text">Home Management. Food and Nutrition. </p>
