d17372676bc8dc7d5bcfc7740d98cfa119b87412
IDOKO EMMANUEL <pidox31102002@gmail.com>
2021-07-07 13:50:04 +0100

    font change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = ({ bgColor }) => {
     return (
         <div className={` ${bgColor} navbar_page`}>
             <nav className="navbar navbar-expand-lg fixed-top navbar-light light">
-                <a style={{ color: 'white' }} className="pl-md-4 navbar-brand" href="#home"><img className="mx-3 ms-0" style={{ height: '40px' }} src={logo} alt="logo" referrerPolicy />Graceworths</a>
+                <a style={{ color: 'white' }} className="pl-md-4 navbar-brand" href="#home"><img className="mx-3 ms-0" style={{ height: '40px' }} src={logo} alt="logo" referrerPolicy />GRACEWORTHS</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
